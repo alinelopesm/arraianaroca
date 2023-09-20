725cc070718436a84339815a5541482040d294ec
alinelopesm <alinealopesm@gmail.com>
2023-09-20 08:10:03 -0300

    refactor: refatora estrutura da aplicação

---
+++ pages/usuario/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Usuario() {
+  return (
+    <div>
+      <Head>
+      <title>Usuario</title>
+      </Head>
+
+      Sou Usuario
+    </div>
+  )
+}
