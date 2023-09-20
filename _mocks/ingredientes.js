dcedf3eecd6a64ba55b038cad9add1bdfcf20137
alinelopesm <alinealopesm@gmail.com>
2023-09-20 14:19:10 -0300

    feat: Adiciona mock de ingredientes

---
+++ _mocks/ingredientes.js
@@ -0,0 +1,29 @@
+export const listAll = () => {
+    return [
+        {
+            "cod_ingrediente": 1,
+            "nome": "Farinha de Trigo"
+        },
+        {
+            "cod_ingrediente": 2,
+            "nome": "Óleo"
+        },
+        {
+            "cod_ingrediente": 3,
+            "nome": "Leite"
+        },
+        {
+            "cod_ingrediente": 4,
+            "nome": "Sal"
+        }
+    ]
+}
+
+export const getById = (id) => {
+    return [
+        {
+            "cod_ingrediente": 4,
+            "nome": "Sal"
+        }
+    ]
+}
