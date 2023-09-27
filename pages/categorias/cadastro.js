429b0d722a065feb4342c97269b688ddf2409e4f
alinelopesm <alinealopesm@gmail.com>
2023-09-27 11:23:59 -0300

    feat: cria esqueleto tela cadastro categorias

---
+++ pages/categorias/cadastro.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { InboxOutlined } from '@ant-design/icons';
+import {
+    Button,
+    Form,
+    Space,
+    Upload,
+    Input,
+    message
+  } from 'antd';
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+
+const props = {
+  name: 'file',
+  multiple: false,
+  onChange(info) {
+    const { status } = info.file;
+    console.log('olha eu no props', info.file);
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
+
+const cadastroCategoria = () => {
+    const [form] = Form.useForm();
+    const onFinish = async (values) => {
+      const file = values?.dragger[0]
+      console.log('Vou cadastrar', values);
+    };
+  
+  
+  return (
+    <Form
+    name="validate_other"
+    {...formItemLayout}
+    onFinish={onFinish}
+    style={{
+      maxWidth: 600,
+    }}
+  >
+    <Form.Item
+        name="nome"
+        label="Nome"
+        tooltip="Como você quer que os outros te chamem?"
+        rules={[
+          {
+            required: true,
+            message: 'Por favor, insira seu nome!',
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+    </Form.Item>
+    <Form.Item label="Dragger">
+      <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+        <Upload.Dragger {...props} name="files" listType="picture">
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+          <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+        </Upload.Dragger>
+      </Form.Item>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{
+        span: 12,
+        offset: 6,
+      }}
+    >
+      <Space>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="reset">reset</Button>
+      </Space>
+    </Form.Item>
+  </Form>
+  );
+};
+export default cadastroCategoria;
